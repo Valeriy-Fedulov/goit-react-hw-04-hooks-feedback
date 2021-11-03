3873e59385eea6e196cbdaba3f836e06c420cf1b
vofm <80212894+Valeriy-Fedulov@users.noreply.github.com>
2021-11-03 17:20:41 +0200

    Update

--- src/components/feedback/feedback.js
+++ src/components/feedback/feedback.js
@@ -1,5 +1,45 @@
-function Feedback() {
-  return <h1>Text</h1>;
+import React from 'react';
+
+class Feedback extends React.Component {
+  onHahdleIncrement = () => {
+    console.log('+1');
+    console.log(this);
+  };
+
+  render() {
+    return (
+      <div>
+        <h2>Please leave feedback</h2>
+        <button
+          type="button"
+          onClick={() => {
+            this.onHahdleIncrement();
+            console.log('Good');
+          }}
+        >
+          Good
+        </button>
+        <button
+          type="button"
+          onClick={() => {
+            this.onHahdleIncrement();
+            console.log('Neutral');
+          }}
+        >
+          Neutral
+        </button>
+        <button
+          type="button"
+          onClick={() => {
+            this.onHahdleIncrement();
+            console.log('Bad');
+          }}
+        >
+          Bad
+        </button>
+      </div>
+    );
+  }
 }
 
 export default Feedback;
